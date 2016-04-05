d463826dd7a4e85f50fd270efabf34d48dcc3cdc
Zhulanov Anton <zhulanov-an@dartit.ru>
2016-04-05 14:14:26 +0500

    improve readme

--- database/app.rb
+++ database/app.rb
@@ -2,6 +2,9 @@ require 'sqlite3'
 
 db = SQLite3::Database.new('test.sqlite')
 
-db.execute("INSERT INTO cars (model, price) VALUES('Gelendvagen', 330000)")
+# db.execute("INSERT INTO cars (model, price) VALUES('Gelendvagen', 330000)")
+db.execute "select * from cars" do |row|
+  puts row.join(' ')
+end
 
 db.close
